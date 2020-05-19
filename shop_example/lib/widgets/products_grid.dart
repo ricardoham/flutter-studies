20310a4e806bd4f29ea8918ea8a161e24238a5f4
Ricardo Manoel <rdomanoel@gmail.com>
2020-05-18 23:17:38 -0300

    changing to create changenotifierprovider

--- shop_example/lib/widgets/products_grid.dart
+++ shop_example/lib/widgets/products_grid.dart
@@ -13,8 +13,8 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, index) => ChangeNotifierProvider(
-        builder: (c) => products[index],
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(
             // products[index].id,
             // products[index].title,
