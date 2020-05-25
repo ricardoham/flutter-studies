2db120b51c405d615cb0d3f0127f1a8fc77d76be
Ricardo Manoel <rdomanoel@gmail.com>
2020-05-25 01:54:22 -0300

    providers in cart widget

--- shop_example/lib/screens/products_overview_screen.dart
+++ shop_example/lib/screens/products_overview_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FILTER_OPTIONS {
   Favorites,
@@ -42,6 +46,16 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                 value: FILTER_OPTIONS.All,
               ),
             ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+            ),
           )
         ],
       ),
