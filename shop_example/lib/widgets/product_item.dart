781436fa86b67d41d48a91f3c98b6d89a8d69423
Ricardo Manoel <rdomanoel@gmail.com>
2020-05-28 23:16:05 -0300

    snack bar

--- shop_example/lib/widgets/product_item.dart
+++ shop_example/lib/widgets/product_item.dart
@@ -53,6 +53,20 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Added item to cart',
+                  textAlign: TextAlign.center,
+                ),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),
